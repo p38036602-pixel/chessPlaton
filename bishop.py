b39d9396a0c29d47178ef771c81c97af3b81890d
p38036602-pixel <p38036602@gmail.com>
2026-03-09 20:50:20 +0500

    Create bishop.py

---
+++ bishop.py
@@ -0,0 +1,28 @@
+from piece import Piece
+
+class Bishop(Piece):
+    def __str__(self):
+        return "♗" if self.color == "white" else "♝"
+    
+    def get_possible_moves(self, board):
+        moves = []
+        directions = [(1, 1), (1, -1), (-1, 1), (-1, -1)]  # диагонали
+        
+        for dx, dy in directions:
+            for step in range(1, 8):
+                new_x = self.x + dx * step
+                new_y = self.y + dy * step
+                
+                if not (0 <= new_x < 8 and 0 <= new_y < 8):
+                    break
+                
+                target = board.grid[new_y][new_x]
+                if target is None:
+                    moves.append((new_x, new_y))
+                elif target.color != self.color:
+                    moves.append((new_x, new_y))
+                    break
+                else:
+                    break
+                    
+        return moves
