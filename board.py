153da80b15b0543d2371e93efee53a8b361088ba
p38036602-pixel <p38036602@gmail.com>
2026-03-17 09:56:32 +0500

    Update board.py

--- board.py
+++ board.py
@@ -30,6 +30,107 @@ class Board:
                     self.place_piece(cls("white", f, 0))
                     self.place_piece(cls("black", f, 7))
 
+    def move_piece(self, from_x, from_y, to_x, to_y):
+        #Перемещает фигуру с проверкой на рокировку
+        piece = self.grid[from_y][from_x]
+        if piece is None:
+            return False
+        
+        # Проверяем, является ли ход рокировкой
+        if piece.__class__.__name__ == "King" and abs(to_x - from_x) == 2:
+            # Рокировка
+            return self._handle_castling(piece, to_x, to_y)
+        
+        # Обычный ход
+        self.grid[from_y][from_x] = None
+        self.grid[to_y][to_x] = piece
+        piece.x, piece.y = to_x, to_y
+        piece.has_moved = True
+        return True
+
+    def _handle_castling(self, king, to_x, to_y):
+        #Обрабатывает рокировку#
+        # Определяем направление рокировки
+        if to_x == 6:  # Короткая рокировка
+            rook = self.grid[to_y][7]
+            if rook and rook.__class__.__name__ == "Rook" and not rook.has_moved:
+                # Перемещаем короля
+                self.grid[king.y][king.x] = None
+                self.grid[to_y][to_x] = king
+                king.x, king.y = to_x, to_y
+                king.has_moved = True
+                
+                # Перемещаем ладью
+                self.grid[to_y][7] = None
+                self.grid[to_y][5] = rook  # Ладья на f1/f8
+                rook.x, rook.y = 5, to_y
+                rook.has_moved = True
+                return True
+                
+        elif to_x == 2:  # Длинная рокировка
+            rook = self.grid[to_y][0]
+            if rook and rook.__class__.__name__ == "Rook" and not rook.has_moved:
+                # Перемещаем короля
+                self.grid[king.y][king.x] = None
+                self.grid[to_y][to_x] = king
+                king.x, king.y = to_x, to_y
+                king.has_moved = True
+                
+                # Перемещаем ладью
+                self.grid[to_y][0] = None
+                self.grid[to_y][3] = rook  # Ладья на d1/d8
+                rook.x, rook.y = 3, to_y
+                rook.has_moved = True
+                return True
+        
+        return False
+
+    def is_in_check(self, color):
+        #Проверяет, находится ли король указанного цвета под шахом
+        # Находим короля
+        king_pos = None
+        for y in range(8):
+            for x in range(8):
+                piece = self.grid[y][x]
+                if piece and piece.__class__.__name__ == "King" and piece.color == color:
+                    king_pos = (x, y)
+                    break
+            if king_pos:
+                break
+        
+        if not king_pos:
+            return False
+        
+        # Проверяем, атакована ли клетка короля
+        return self.is_square_attacked(king_pos, color)
+
+    def is_square_attacked(self, square, defending_color):
+        #Проверяет, атакована ли клетка фигурами противника"""
+        target_x, target_y = square
+        attacking_color = "black" if defending_color == "white" else "white"
+        
+        for y in range(8):
+            for x in range(8):
+                piece = self.grid[y][x]
+                if piece and piece.color == attacking_color:
+                    # Получаем все возможные ходы фигуры (без проверки на шах)
+                    if piece.__class__.__name__ == "Pawn":
+                        # Особый случай для пешки (бьет по диагонали)
+                        if self._is_pawn_attacking(piece, target_x, target_y):
+                            return True
+                    else:
+                        moves = piece.get_possible_moves(self)
+                        if (target_x, target_y) in moves:
+                            return True
+        return False
+
+    def _is_pawn_attacking(self, pawn, target_x, target_y):
+        #Проверяет, атакует ли пешка указанную клетку"""
+        direction = 1 if pawn.color == "white" else -1
+        # Пешка атакует по диагонали вперед
+        return (target_y == pawn.y + direction and 
+                abs(target_x - pawn.x) == 1)
+
     def __str__(self):
         rows = []
         for row_idx in range(7, -1, -1):
